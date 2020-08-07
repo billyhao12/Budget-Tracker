5e2c8402d8f70502c6882c24a9c08f67453c6779
Billy Hao <billyhao12@gmail.com>
2020-08-06 18:27:01 -0700

    Added code for indexedDB during offline use

---
+++ public/db.js
@@ -0,0 +1,24 @@
+const request = window.indexedDB.open("budget", 1);
+
+let db;
+
+request.onupgradeneeded = event => {
+  const db = event.target.result;
+
+  const transactions = db.createObjectStore("transactions", {autoIncrement: true});
+  
+}
+
+request.onsuccess = () => {
+  db = request.result;
+
+}
+
+function saveRecord(record) {
+  console.log(record);
+
+  const transaction = db.transaction( ["transactions"], "readwrite" );
+  const transactionStore = transaction.objectStore("transactions");
+
+  transactionStore.add(record);
+}
