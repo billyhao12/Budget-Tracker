94d52861d327619408f71cacc6155a1c718e39c8
Billy Hao <billyhao12@gmail.com>
2020-08-08 09:45:31 -0700

    App now bulk adds items from IndexedDB after going online

--- public/db.js
+++ public/db.js
@@ -5,7 +5,7 @@ let db;
 request.onupgradeneeded = event => {
   const db = event.target.result;
 
-  const transactions = db.createObjectStore("transactions", {autoIncrement: true});
+  db.createObjectStore("transactions", {autoIncrement: true});
   
 }
 
@@ -15,10 +15,37 @@ request.onsuccess = () => {
 }
 
 function saveRecord(record) {
-  console.log(record);
 
   const transaction = db.transaction( ["transactions"], "readwrite" );
   const transactionStore = transaction.objectStore("transactions");
 
   transactionStore.add(record);
 }
+
+function checkDatabase() {
+
+  // Access data in IndexedDB
+  const transaction = db.transaction( ["transactions"], "readwrite" );
+  const transactionStore = transaction.objectStore("transactions");
+
+  const getRequest = transactionStore.getAll();
+  getRequest.onsuccess = () => {
+    
+    // Clear data from IndexedDB
+    transactionStore.clear();
+    
+    // Submit to MongoDB
+    fetch("/api/transaction/bulk", {
+      method: "POST",
+      body: JSON.stringify(getRequest.result),
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json"
+      }
+    });
+
+  }
+
+}
+
+window.addEventListener("online", checkDatabase);
